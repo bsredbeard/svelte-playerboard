f0b8427db576b341b395f682a8d7c748c26d95e5
Bill Smith <billsmithdev@gmail.com>
2018-11-03 12:23:37 -0400

    Restore position on load, reset autoId on loa

--- src/autoid.js
+++ src/autoid.js
@@ -3,24 +3,28 @@
  * Create a new set of AutoIDs
  * @class AutoId
  */
-export default function AutoId(){
-    let curr = 0;
-    /**
-     * Retrieve the next auto-id
-     * @method
-     * @name next
-     * @memberof AutoId
-     * @instance
-     * @returns {number}
-     */
-    this.next = () => curr++,
-    /**
-     * Reset this auto-id instance to its original starting value
-     * @method
-     * @name reset
-     * @memberof AutoId
-     * @instance
-     * @returns {void}
-     */
-    this.reset = () => { curr = 0; }
+export default class AutoId{
+    constructor(){
+        let curr = 0;
+        /**
+         * Retrieve the next auto-id
+         * @method
+         * @name next
+         * @memberof AutoId
+         * @instance
+         * @returns {number}
+         */
+        this.next = () => curr++;
+        /**
+         * Reset this auto-id instance to its original starting value
+         * @method
+         * @name reset
+         * @memberof AutoId
+         * @instance
+         * @returns {void}
+         */
+        this.set = (val) => {
+            curr = val ? val : 0;
+        };
+    }
 }
